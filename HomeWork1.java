4cc236d445bed1ee1cc526ad1be90cb3163742c1
ITrushina <90238726+ITrushina@users.noreply.github.com>
2021-09-12 18:34:06 +0300

    Измененный файл ДЗ1

--- HomeWork1.java
+++ HomeWork1.java
@@ -1,41 +1,49 @@
+/*
+ * Java 1. Home work 1
+ *
+ * @author Trushina Irina
+ * @version 10.09.2021
+ */
 class HomeWork1 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        checkSumSign(4, 3);
+        checkSumSign(3, -3);
+        checkSumSign(-3, -4);
+        printColor(-1);
+        printColor(0);
+        printColor(1);
+        printColor(99);
+        printColor(100);
+        printColor(101);
+        compareNumbers(5, 2);
+        compareNumbers(5, 5);
+        compareNumbers(2, 5);
     }
-    public static void printThreeWords() {
+
+    static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-    public static void checkSumSign() {
-        int a = -4, b = 4;
-        if (a + b >= 0) {
-            System.out.println("sum is positive");
-        } else {
-            System.out.println("Sum is negative");
-        }
+
+    static void checkSumSign(int a, int b) {
+        System.out.println("Sum is " + (a + b >= 0? "positive" : "negative"));
     }
-    public static void printColor() {
-        int value = -5;
+
+    static void printColor(int value) {
         if (value <= 0) {
             System.out.println("Red");
         }
         if (value > 0 && value <= 100){
             System.out.println("Yellow");
         } 
-        if (value >= 100){
+        if (value > 100){
             System.out.println("Green");
         }
-    } 
-    public static void compareNumbers() {
-        int a = 585, b = 35;
-        if (a >= b) {
-            System.out.println("a>=b");
-        } else {
-            System.out.println("a<b");
-        }
+    }
+
+    static void compareNumbers(int a, int b) {
+        System.out.println(a >= b? "a>=b" : "a<b");
     }
 }
