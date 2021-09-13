8b5bdef0b61df45b6102a4ee93925f16e750c37e
ITrushina <90238726+ITrushina@users.noreply.github.com>
2021-09-13 09:55:45 +0300

    ДЗ 2 (#2)
    * ДЗ 2
    
    * Исправленное ДЗ2

---
+++ HomeWork2.java
@@ -0,0 +1,56 @@
+/*
+ * Java 1. Home work 2
+ *
+ * @author Trushina Irina
+ * @version 12.09.2021
+ */
+class HomeWork2 {
+    public static void main(String[] args) {
+        System.out.println(sumBetween(1, 5));
+        System.out.println(sumBetween(10, 0));
+        System.out.println(sumBetween(10, 5));
+        System.out.println(sumBetween(10, 10));
+        System.out.println(sumBetween(10, 20));
+        System.out.println(" ");
+        positiveOrNegative (-5);
+        positiveOrNegative (0);
+        positiveOrNegative (5);
+        System.out.println(" ");
+        System.out.println(returnPositiveOrNegative (-5));
+        System.out.println(returnPositiveOrNegative (0));
+        System.out.println(returnPositiveOrNegative (5));
+        System.out.println(" ");
+        printText ("Hello", 5);
+        System.out.println(" ");
+        System.out.println(leapYear(1763));
+        System.out.println(leapYear(1764));
+        System.out.println(leapYear(1800));
+        System.out.println(leapYear(2000));
+    }
+
+    static boolean sumBetween (int a, int b) {
+        return (a + b) >= 10 && (a + b) <= 20;
+    }
+
+    static void positiveOrNegative (int a) {
+        System.out.println("Number is " + (a >= 0? "positive" : "negative"));
+    }
+
+    static boolean returnPositiveOrNegative (int a) {
+        return a < 0;
+    }
+
+    static void printText (String a, int b) {
+        for (int i = 0; i < b; i++){
+            System.out.println(a);
+        }
+    }
+
+    static boolean leapYear (int a) {
+        boolean result = false;
+        if (((a % 4) == 0 && (a % 100) != 0 && (a % 400) != 0) || ((a % 4) == 0 && (a % 400) == 0)) {
+            result = true;
+        }
+        return result;
+    }
+}
